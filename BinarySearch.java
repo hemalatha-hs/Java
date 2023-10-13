0f6de83a71de1cbc481183fe7c852437415109bc
hema2820 <hemalathahs2820@gmail.com>
2023-10-13 15:50:45 +0530

    updated

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,13 +1,19 @@
 class BinarySearch{  
-    public static void binarySearch(int arr[], int first, int last, int key){  
+    public static void binarySearch(int arr[], int first, int last, int key)
+    {  
       int mid = (first + last)/2;  
-      while( first <= last ){  
-         if ( arr[mid] < key ){  
+      while( first <= last )
+      {  
+         if ( arr[mid] < key )
+         {  
            first = mid + 1;     
-         }else if ( arr[mid] == key ){  
+         }
+         else if ( arr[mid] == key )
+         {  
            System.out.println("Element is found at index: " + mid);  
            break;  
-         }else{  
+         }
+         else{  
             last = mid - 1;  
          }  
          mid = (first + last)/2;  
